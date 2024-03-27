a38df2ed290fead0133957fbed95c1caeb99b7c7
Fatima <fatima.abdallah@mu.edu.lb>
2024-03-27 22:31:36 +0200

    Lesson 5 Complete insert and update

--- app/src/main/java/com/example/contacts/ContactDataSource.java
+++ app/src/main/java/com/example/contacts/ContactDataSource.java
@@ -1,14 +1,19 @@
 package com.example.contacts;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class ContactDataSource {
     SQLiteDatabase database;
     ContactDBHelper dbHelper;
 
-    public ContactDataSource(Context context){
+    public ContactDataSource(Context context) {
         dbHelper = new ContactDBHelper(context);
     }
 
@@ -16,7 +21,62 @@ public class ContactDataSource {
         database = dbHelper.getWritableDatabase();
     }
 
-    public void close(){
+    public void close() {
         dbHelper.close();
+    }
+
+    public boolean insertContact(Contact c) {
+        boolean didSucceed = false;
+        try {
+            ContentValues initialValues = new ContentValues();
+            initialValues.put("contactname", c.getContactName());
+            initialValues.put("streetaddress", c.getStreetAddress());
+            initialValues.put("city", c.getCity());
+            initialValues.put("state", c.getState());
+            initialValues.put("zipcode", c.getZipcode());
+            initialValues.put("phonenumber", c.getPhoneNumber());
+            initialValues.put("cellnumber", c.getCellNumber());
+            initialValues.put("email", c.getEMail());
+            initialValues.put("birthday", String.valueOf(c.getBirthday().getTimeInMillis()));
+            didSucceed = database.insert("contact", null, initialValues) > 0;
+        } catch (Exception e) {
+            Log.d("My Database", "Something went wrong!");
+        }
+        return didSucceed;
+    }
+
+    public boolean updateContact(Contact c) {
+        boolean didSucceed = false;
+        try {
+            long rowID = c.getContactID();
+            ContentValues updatedValues = new ContentValues();
+            updatedValues.put("contactname", c.getContactName());
+            updatedValues.put("streetaddress", c.getStreetAddress());
+            updatedValues.put("city", c.getCity());
+            updatedValues.put("state", c.getState());
+            updatedValues.put("zipcode", c.getZipcode());
+            updatedValues.put("phonenumber", c.getPhoneNumber());
+            updatedValues.put("cellnumber", c.getCellNumber());
+            updatedValues.put("email", c.getEMail());
+            updatedValues.put("birthday", String.valueOf(c.getBirthday().getTimeInMillis()));
+            didSucceed = database.update("contact", updatedValues, "_id = " + rowID, null) > 0;
+        } catch (Exception e) {
+        }
+        return didSucceed;
+    }
+
+    public int getLastContact(){
+        int lastId;
+        try{
+            String query = "Select MAX(_id) from contact";
+            Cursor cursor = database.rawQuery(query,null);
+            cursor.moveToFirst();
+            lastId = cursor.getInt(0);
+            cursor.close();
+        }
+        catch (Exception e){
+            lastId = -1;
+        }
+        return lastId;
     }
 }
--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.contacts;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -13,10 +14,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.SaveDateListener {
 
+    Contact currentContact;
     ImageButton listImageButton, mapImageButton, settingsImageButton, saveButton;
     Button changeBirthdayButton;
     ToggleButton editToggle;
@@ -34,10 +39,160 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         initToggleButton();
         setForEditing(false);
         initChangeBirthdayButton();
+        currentContact = new Contact();
+        initTextChangedEvents();
+        initSaveButton();
+    }
+
+    private void initSaveButton() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean wasSuccessful = false;
+                ContactDataSource dataSource = new ContactDataSource(MainActivity.this);
+                try {
+                    dataSource.open();
+                    if (currentContact.getContactID() == -1) {
+                        wasSuccessful = dataSource.insertContact(currentContact);
+                        if(wasSuccessful){
+                            int newId = dataSource.getLastContact();
+                            currentContact.setContactID(newId);
+                        }
+                    } else {
+                        wasSuccessful = dataSource.updateContact(currentContact);
+                    }
+                } catch (Exception e) {
+                }
+                if (wasSuccessful) {
+                    editToggle.toggle();
+                    setForEditing(false);
+                }
+            }
+        });
+    }
+
+    private void initTextChangedEvents() {
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.d("Before", nameEditText.getText().toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d("on", nameEditText.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.d("After", nameEditText.getText().toString());
+                currentContact.setContactName(nameEditText.getText().toString());
+            }
+        });
+        addressEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setStreetAddress(addressEditText.getText().toString());
+            }
+        });
+        cityEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setCity(cityEditText.getText().toString());
+            }
+        });
+        stateEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setState(stateEditText.getText().toString());
+            }
+        });
+        zipEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setZipcode(zipEditText.getText().toString());
+            }
+        });
+        homeEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setPhoneNumber(homeEditText.getText().toString());
+            }
+        });
+        cellEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setCellNumber(cellEditText.getText().toString());
+            }
+        });
+        emaiEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                currentContact.setEMail(emaiEditText.getText().toString());
+            }
+        });
+        homeEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        cellEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
 
     private void initToggleButton() {
-        editToggle.setOnClickListener(new View.OnClickListener(){
+        editToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setForEditing(editToggle.isChecked());
@@ -56,7 +211,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         emaiEditText.setEnabled(checked);
         changeBirthdayButton.setEnabled(checked);
         saveButton.setEnabled(checked);
-        if(checked) {
+        if (checked) {
             nameEditText.requestFocus();
             InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             imm.showSoftInput(nameEditText, 0);
@@ -64,17 +219,17 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     }
 
     private void initChangeBirthdayButton() {
-        changeBirthdayButton.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View view) {
-                        FragmentManager fm =getSupportFragmentManager();
-                        DatePickerDialog dialog = new DatePickerDialog();
-                        dialog.show(fm, "DatePicker");
-                    }
-                });
+        changeBirthdayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                DatePickerDialog dialog = new DatePickerDialog();
+                dialog.show(fm, "DatePicker");
+            }
+        });
     }
 
-    private void initLayoutComponents(){
+    private void initLayoutComponents() {
         listImageButton = findViewById(R.id.imageButtonList);
         mapImageButton = findViewById(R.id.imageButtonMap);
         settingsImageButton = findViewById(R.id.imageButtonSettings);
@@ -84,7 +239,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         cityEditText = findViewById(R.id.editCity);
         stateEditText = findViewById(R.id.editState);
         zipEditText = findViewById(R.id.editZip);
-        homeEditText  = findViewById(R.id.editHome);
+        homeEditText = findViewById(R.id.editHome);
         cellEditText = findViewById(R.id.editCell);
         emaiEditText = findViewById(R.id.editEmail);
         changeBirthdayButton = findViewById(R.id.btnBirthday);
@@ -95,5 +250,6 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
     @Override
     public void didFinishDatePickerDialog(Calendar selectedDate) {
         birthdayText.setText(DateFormat.format("dd/MM/yyyy", selectedDate));
+        currentContact.setBirthday(selectedDate);
     }
 }
